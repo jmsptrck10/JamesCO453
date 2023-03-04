93b746e4902cd937b48b0aa3321582885ed5f236
jmsptrck10 <22206838@bucks.ac.uk>
2023-03-04 19:12:32 +0000

    doneeeeeeeeeeeeeeeee

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -2,125 +2,202 @@
 
 namespace ConsoleAppProject.App01
 {
-/// <summary>
-/// This class converts a distance between miles, feet and metres
-/// </summary>
-/// <author>
-/// Derek
-/// </author>
-public class DistanceConverter
-{
-private const double MILES_TO_FEET = 5280.0;
-private const double MILES_TO_METRES = 1609.34;
-
-    private string fromUnit;
-    private string toUnit;
-    private double fromDistance;
-    private double toDistance;
-
-    public DistanceConverter()
+    /// <summary>
+    /// This class provides a distance converter that allows the user
+    /// to convert a distance between miles, feet and metres.
+    /// </summary>
+    /// <author>
+    /// James Patrick Arellano
+    /// </author>
+    
+    public class DistanceConverter
     {
-        Console.WriteLine("Distance Converter");
-        Console.WriteLine("By Derek");
-    }
+        // Conversion factors for miles to feet and metres
+        private const double MILES_TO_FEET = 5280.0;
+        private const double MILES_TO_METRES = 1609.34;
+        private const double FEET_TO_METRES = 0.3048;
+        private const double METRES_TO_FEET = 3.28084;
+        
 
-    public void Run()
-    {
-        Console.WriteLine("Select the unit to convert from:");
-        fromUnit = SelectUnit();
+        // Private fields to store user inputs and conversion results
+        private string fromUnit;
+        private string toUnit;
+        private double fromDistance;
+        private double toDistance;
 
-        Console.WriteLine("Select the unit to convert to:");
-        toUnit = SelectUnit();
 
+        /// Constructor for DistanceConverter class.
+        /// Displays a welcome message to the console.
+
+        public DistanceConverter()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("==========================");
+            Console.WriteLine("Distance Converter");
+            Console.WriteLine("By James Patrick Arellano");
+            Console.WriteLine("==========================");
+        }
+
+
+        /// Main method that runs the distance converter.
+        /// Prompts the user for input, converts the distance,
+        /// and prints the result to the console.
+
+        public void Run()
+        {
+            Console.WriteLine("Select the unit to convert from:");
+            fromUnit = GetValidUnit();
+
+            Console.WriteLine("Select the unit to convert to:");
+            toUnit = GetValidUnit();
+
+            while (true)
+            {
         Console.WriteLine($"Enter the distance in {fromUnit}:");
         if (!double.TryParse(Console.ReadLine(), out fromDistance))
         {
             Console.WriteLine("Error: Invalid distance entered");
-            return;
+            continue;
         }
-
-        ConvertDistance();
-        Print();
-    }
-
-    private string SelectUnit()
-    {
-        Console.WriteLine("1. Miles");
-        Console.WriteLine("2. Feet");
-        Console.WriteLine("3. Metres");
-        Console.Write("Enter your choice: ");
-
-        switch (Console.ReadLine())
+            else if (fromDistance <= 0)
         {
-            case "1":
-                return "miles";
-            case "2":
-                return "feet";
-            case "3":
-                return "metres";
-            default:
-                Console.WriteLine("Error: Invalid unit choice");
-                return null;
-        }
-    }
-
-    private void ConvertDistance()
-    {
-        switch (fromUnit)
-        {
-            case "miles":
-                toDistance = fromDistance * GetConversionFactor(toUnit, "feet") * GetConversionFactor("miles", "feet");
-                break;
-            case "feet":
-                toDistance = fromDistance * GetConversionFactor(toUnit, "feet");
-                break;
-            case "metres":
-                toDistance = fromDistance * GetConversionFactor(toUnit, "metres");
-                break;
-        }
-    }
-
-    private double GetConversionFactor(string fromUnit, string toUnit)
-    {
-        switch (fromUnit)
-        {
-            case "miles":
-                switch (toUnit)
-                {
-                    case "feet":
-                        return MILES_TO_FEET;
-                    case "metres":
-                        return MILES_TO_METRES;
-                }
-                break;
-            case "feet":
-                switch (toUnit)
-                {
-                    case "miles":
-                        return 1.0 / MILES_TO_FEET;
-                    case "metres":
-                        return 0.3048;
-                }
-                break;
-            case "metres":
-                switch (toUnit)
-                {
-                    case "miles":
-                        return 1.0 / MILES_TO_METRES;
-                    case "feet":
-                        return 3.28084;
-                }
-                break;
+            Console.WriteLine("Error: Distance must be greater than 0");
+            continue;
         }
 
-        Console.WriteLine("Error: Invalid unit choice");
-        return 0.0;
-    }
-
-    private void Print()
-    {
-        Console.WriteLine($"{fromDistance} {fromUnit} is {toDistance} {toUnit}");
-    }
+            ConvertDistance();
+            PrintConversion();
+            break;
+        }
 }
 
+   
+        /// Prompts the user to select a valid unit of measurement.
+        /// <returns>The selected unit as a string.</returns>
+        private string GetValidUnit()
+        {
+            Console.WriteLine("1. Miles");
+            Console.WriteLine("2. Feet");
+            Console.WriteLine("3. Metres");
+
+            Console.Write("Enter your choice: ");
+
+            string input = Console.ReadLine().Trim().ToLower();
+            switch (input)
+            {
+                case "1":
+                case "miles":
+                    return "miles";
+                case "2":
+                case "feet":
+                    return "feet";
+                case "3":
+                case "metres":
+                    return "metres";
+                default:
+
+                    Console.WriteLine("Error: Invalid unit choice");
+                    return GetValidUnit();
+            }
+        }
+
+ 
+        /// Converts the distance from the "fromUnit" to the "toUnit"
+        /// using the appropriate conversion factor.
+  
+        private void ConvertDistance()
+        {
+             switch (fromUnit)
+            {
+                case "miles":
+                    if (toUnit == "feet")
+                    {
+                        toDistance = fromDistance * MILES_TO_FEET;
+                    }
+                    else if (toUnit == "metres")
+                    {
+                        toDistance = fromDistance * MILES_TO_METRES;
+                    }
+                    break;
+
+                case "feet":
+                   if (toUnit == "miles")
+                    {
+                        toDistance = fromDistance / MILES_TO_FEET;
+                    }
+                    else if (toUnit == "metres")
+                    {
+                        toDistance = fromDistance * FEET_TO_METRES;
+                    }
+                    break;
+
+                case "metres":
+                   if (toUnit == "miles")
+                    {
+                        toDistance = fromDistance / MILES_TO_METRES;
+                    }
+                    else if (toUnit == "feet")
+                    {
+                        toDistance = fromDistance * METRES_TO_FEET;
+                    }
+                    break;
+            }
+        }
+
+    
+        /// Returns the conversion factor for converting from one unit
+        /// to another unit
+        /// <param name="fromUnit">The unit to convert from.</param>
+        /// <param name="toUnit">The unit to convert to.</param>
+        /// <returns>The conversion factor
+
+
+private double GetConversionFactor(string fromUnit, string toUnit)
+{
+    switch (fromUnit)
+    {
+        case "miles":
+            switch (toUnit)
+            {
+                case "feet":
+                    return 1.0 * MILES_TO_FEET;
+                case "metres":
+                    return 1.0 * MILES_TO_METRES;
+            }
+            break;
+        case "feet":
+            switch (toUnit)
+            {
+                case "miles":
+                    return 1.0 / MILES_TO_FEET;
+                case "metres":
+                    return 1.0 * FEET_TO_METRES;
+            }
+            break;
+        case "metres":
+            switch (toUnit)
+            {
+                case "miles":
+                    return 1.0 / MILES_TO_METRES;
+                case "feet":
+                    return 1.0 * METRES_TO_FEET;
+            }
+            break;
+    }
+
+    // If an invalid unit choice was entered, print an error message and return 0.0
+    
+    Console.WriteLine("Error: Invalid unit choice");
+    return 0.0;
+}
+
+
+///This method prints the conversion result to the console
+        private void PrintConversion()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{fromDistance} {fromUnit} is {toDistance} {toUnit}");
+        }
+    }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -9,21 +9,19 @@ namespace ConsoleAppProject
     /// The main method in this class is called first
     /// when the application is started.  It will be used
     /// to start App01 to App05 for CO453 CW1
-    /// 
-    /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
     /// </summary>
+    /// <author>
+    /// James Patrick Arellano
+    /// </author>
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Black;
-            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
 
             Console.WriteLine();
             Console.WriteLine(" =================================================");
             Console.WriteLine("    BNU CO453 Applications Programming 2022-2023! ");
-            Console.WriteLine("        by James Patrick                                 ");
             Console.WriteLine(" =================================================");
             Console.WriteLine();
 
