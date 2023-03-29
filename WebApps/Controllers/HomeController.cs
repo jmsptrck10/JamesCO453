e1aeeede7450ad678c0d6b625baf2680584f81c9
jmsptrck10 <22206838@bucks.ac.uk>
2023-03-29 08:55:12 +0100

    ./.

--- WebApps/Controllers/HomeController.cs
+++ WebApps/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WebApps.Models;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace WebApps.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly NewsFeed _newsFeed;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _newsFeed = new NewsFeed();
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_newsFeed.GetPosts());
         }
 
         public IActionResult Privacy()
